b8d09654a83ec837bbf0aaee947230eeb7711215
maulanasechan <maulanasechan123@gmail.com>
2023-08-02 22:28:58 +0700

    STYLE: STYLING FOR IPAD IN MIN WIDTH: 550PX

--- Profile/script.js
+++ Profile/script.js
@@ -30,12 +30,38 @@ function togglePopUp() {
 function toggleEdit() {
   var avatarContainer = document.querySelector(`.avatarContainer`);
   var editContainer = document.querySelector(`.editContainer`);
+  var inputFile = document.querySelector(`.file-input-label`);
+  var backButton = document.querySelector(`.backButton`);
   var hasEdit = avatarContainer.classList.contains("deactive");
   if (hasEdit) {
     avatarContainer.classList.remove("deactive");
     editContainer.classList.add("deactive");
+    inputFile.classList.add("deactive");
+    backButton.setAttribute(
+      "onclick",
+      "window.location.href='../Homepage/index.html'"
+    );
   } else {
     avatarContainer.classList.add("deactive");
     editContainer.classList.remove("deactive");
+    inputFile.classList.remove("deactive");
+    backButton.setAttribute("onclick", "toggleEdit()");
   }
 }
+
+// Get the file input element and icon element
+const fileInput = document.getElementById("fileInput");
+const icon = document.querySelector(".file-input-icon");
+
+// Add click event listener to the icon
+icon.addEventListener("click", () => {
+  // Trigger the click event on the file input
+  fileInput.click();
+});
+
+// Add change event listener to the file input to handle selected file
+fileInput.addEventListener("change", (event) => {
+  const selectedFile = event.target.files[0];
+  // Do something with the selected file, such as displaying its name or processing it
+  console.log("Selected file:", selectedFile.name);
+});
